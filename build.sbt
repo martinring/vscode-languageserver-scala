c8a4e3fd8e3dad680069e544fe5cef2b7087cc20
Martin Ring <martin.ring@dfki.de>
2016-10-27 11:29:44 +0200

    textdocument stream

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := "vscode-languageserver"
-version := "0.4.8"
+version := "0.4.9"
 organization := "net.flatmap"
 scalaVersion := "2.11.8"
 
--- src/main/scala/net/flatmap/vscode/languageserver/TextDocuments.scala
+++ src/main/scala/net/flatmap/vscode/languageserver/TextDocuments.scala
@@ -3,44 +3,145 @@ package net.flatmap.vscode.languageserver
 import java.net.URI
 
 import akka.NotUsed
-import akka.actor.ActorRefFactory
-import akka.stream.actor.ActorPublisher
-import akka.stream.actor.ActorPublisherMessage.{Cancel, Request}
 import akka.stream.scaladsl.Source
-
-/*private class TextDocumentActor extends ActorPublisher[TextDocumentItem] {
-  def receive: Receive = {
-    case item: TextDocumentItem =>
-      context.become(initialized(item, totalDemand > 0))
-      if (totalDemand > 0) onNext(item)
-  }
-
-  def initialized(item: TextDocumentItem, delivered: Boolean = false): Receive = {
-    case (version: Int, changes: Seq[TextDocumentContentChangeEvent]) =>
-      for (last <- changes.lastOption) {
-        val next = item.copy(text = last.text, version = version)
-        context.become(initialized(next,totalDemand > 0))
-        if (totalDemand > 0) onNext(next)
-      }
-    case Request(_) =>
-      if (!delivered) {
-        onNext(item)
-        context.become(initialized(item,true))
-      }
-    case Cancel => context.stop(self)
-  }
-}
-
-private class TextDocumentsActor extends ActorPublisher[Source[TextDocumentItem,NotUsed]] {
-  def receive: Receive = state(Map.empty)
-
-  def state(items: Map[URI,TextDocumentItem]): Receive = {
-    case _ =>
-  }
-}
+import org.reactivestreams.{Publisher, Subscriber, Subscription}
 
 trait TextDocuments extends LanguageServer {
   override def textDocumentSyncKind: TextDocumentSyncKind = TextDocumentSyncKind.None
-  def actorFactory: ActorRefFactory
-  def textDocuments: Source[Source[TextDocumentItem,NotUsed],NotUsed] =
-}*/
+
+  def textDocuments: Source[(URI,Source[TextDocumentItem,NotUsed]),NotUsed] =
+    Source.fromPublisher(DocumentPublisher)
+
+  private class DocumentPublisher(var state: TextDocumentItem) extends Publisher[TextDocumentItem] {
+    var subscribers =
+      Map.empty[Subscriber[_ >: TextDocumentItem], (Long,Boolean)]
+
+    def subscribe(s: Subscriber[_ >: TextDocumentItem]): Unit = {
+      if (closed) {
+        s.onSubscribe(new Subscription {
+          def cancel(): Unit = ()
+          def request(n: Long): Unit = ()
+        })
+        s.onComplete()
+      } else {
+        subscribers += s -> (0,false)
+        s.onSubscribe(new Subscription {
+          def cancel() =
+            if (subscribers.contains(s)) {
+              subscribers -= s
+              s.onComplete()
+            }
+
+          def request(n: Long) =
+            for {
+              (m, delivered) <- subscribers.get(s)
+            } {
+              subscribers += s -> (
+                if (!delivered) {
+                  s.onNext(state)
+                  (m + n - 1, true)
+                } else (m + n, true)
+                )
+            }
+        })
+      }
+    }
+
+    def change(textDocument: VersionedTextDocumentIdentifier,
+               contentChanges: Seq[TextDocumentContentChangeEvent]): Unit = {
+      for {
+        last <- contentChanges.lastOption
+      } {
+        state = state.copy(
+          text = last.text,
+          version = textDocument.version
+        )
+        subscribers = (subscribers.map {
+          case (s,(0l,_)) => (s,(0l,false))
+          case (s,(n,_)) =>
+            s.onNext(state)
+            (s,(n-1,true))
+        }).toMap
+      }
+    }
+
+    var closed = false
+
+    def close() = {
+      closed = true
+      subscribers.keys.foreach(_.onComplete())
+      subscribers = Map.empty
+    }
+  }
+
+  private object DocumentPublisher extends Publisher[(URI,Source[TextDocumentItem,NotUsed])] {
+    var documents = Map.empty[URI,DocumentPublisher]
+
+    var subscribers =
+      Map.empty[Subscriber[_ >: (URI,Source[TextDocumentItem,NotUsed])],
+                (Long,Seq[(URI,Source[TextDocumentItem,NotUsed])])]
+
+    def subscribe(s: Subscriber[_ >: (URI,Source[TextDocumentItem,NotUsed])]): Unit = {
+      subscribers += s -> (0,Seq.empty)
+      s.onSubscribe(new Subscription {
+        def cancel() =
+          if (subscribers.contains(s)) {
+            subscribers -= s
+            s.onComplete()
+          }
+        def request(n: Long) =
+          for {
+            (m,buf) <- subscribers.get(s)
+          } {
+            subscribers += s -> (if (buf.nonEmpty) {
+              buf.take((n + m).toInt).foreach(s.onNext)
+              (Math.max(0, m + n - (buf.length)), buf.drop((n + m).toInt))
+            }
+            else (m + n, buf))
+          }
+      })
+    }
+
+    def open(textDocumentItem: TextDocumentItem) = {
+      val pub = new DocumentPublisher(textDocumentItem)
+      val src = textDocumentItem.uri -> Source.fromPublisher(pub)
+      documents += textDocumentItem.uri -> pub
+      subscribers = subscribers.map {
+        case (s,(0l,buf)) => (s,(0l,buf :+ src))
+        case (s,(n,buf)) =>
+          val nbuf = buf :+ src
+          nbuf.take(n.toInt).foreach(s.onNext)
+          (s,(Math.max(0,n - nbuf.length), nbuf.drop(n.toInt)))
+      }.toMap
+    }
+
+    def change(textDocument: VersionedTextDocumentIdentifier,
+               contentChanges: Seq[TextDocumentContentChangeEvent]) = {
+      documents.get(textDocument.uri).foreach { publisher =>
+        publisher.change(textDocument,contentChanges)
+      }
+    }
+
+    def close(uri: URI) = {
+      documents.get(uri).foreach(_.close())
+      documents -= uri
+    }
+  }
+
+  override def didOpen(textDocument: TextDocumentItem): Unit = {
+    DocumentPublisher.open(textDocument)
+  }
+
+  override def didChange(textDocument: VersionedTextDocumentIdentifier,
+                contentChanges: Seq[TextDocumentContentChangeEvent]): Unit = {
+    DocumentPublisher.change(textDocument,contentChanges)
+  }
+
+  override def didSave(textDocument: TextDocumentIdentifier): Unit = {
+    // TODO: Signal Save events
+  }
+
+  override def didClose(textDocument: TextDocumentIdentifier): Unit = {
+    DocumentPublisher.close(textDocument.uri)
+  }
+}
